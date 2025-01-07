cb600fcf09d0b13a1b955d388b4c8ce7cff63940
Matthias Kay <matthias.kay@hlag.com>
2025-01-07 09:09:36 +0100

    BASE-APPLICATION

--- src/rds_proxy_password_rotatation/adapter/aws_secrets_manager.py
+++ src/rds_proxy_password_rotatation/adapter/aws_secrets_manager.py
@@ -4,8 +4,8 @@ from rds_proxy_password_rotatation.services import SecretsManagerService
 
 
 class AwsSecretsManagerService(SecretsManagerService):
-    def __init__(self):
-        self.client = boto3.client('secretsmanager')
+    def __init__(self, boto3_secrets_manager=boto3.client('secretsmanager')):
+        self.client = boto3_secrets_manager
 
     def is_rotation_enabled(self, secret_id: str) -> bool:
         metadata = self.client.describe_secret(SecretId=secret_id)
--- tests/rds_proxy_password_rotatation/adapter/test_aws_secrets_manager_infra.py
+++ tests/rds_proxy_password_rotatation/adapter/test_aws_secrets_manager_infra.py
@@ -1,24 +1,59 @@
+import uuid
 from unittest import TestCase
 
-from localstack_utils.localstack import startup_localstack, stop_localstack
+import boto3
 
 from rds_proxy_password_rotatation.adapter.aws_secrets_manager import AwsSecretsManagerService
 
 class TestAwsSecretsManagerService(TestCase):
+    secretname_without_rotation = f'secret_without_rotation_enabled_{uuid.uuid4()}'
+    secretname_with_rotation = f'secret_with_rotation_enabled_{uuid.uuid4()}'
+
     @classmethod
     def setUpClass(self):
-        startup_localstack()
+        self.secretsmanager = boto3.client(service_name='secretsmanager', endpoint_url='http://localhost:4566',aws_access_key_id='test',
+                                      aws_secret_access_key='test')
+        secret = self.secretsmanager.create_secret(
+            Name=self.secretname_without_rotation
+        )
+
+        secret = self.secretsmanager.create_secret(
+            Name=self.secretname_with_rotation
+        )
+
+        """ secretsmanager.rotate_secret(
+            SecretId=secret['ARN'],
+            RotationRules={
+                'AutomaticallyAfterDays': 123,
+                'Duration': '3h',
+                'ScheduleExpression': 'rate(10 days)'
+            },
+            RotateImmediately=False
+        ) """
 
     @classmethod
     def tearDownClass(self):
-        stop_localstack()
-        return super().tearDown()
+        self.secretsmanager.delete_secret(
+            SecretId=self.secretname_without_rotation,
+        )
+        self.secretsmanager.delete_secret(
+            SecretId=self.secretname_with_rotation,
+        )
 
-    def test_should_return_rotation_step_when_to_rotation_step_given_create_secret(self):
+    def test_should_return_false_when_is_rotation_enabled_given_secret_has_rotation_disabled(self):
         # Given
 
         # When
-        rotation_step = AwsSecretsManagerService.to_rotation_step("create_secret")
+        result = AwsSecretsManagerService(self.secretsmanager).is_rotation_enabled(self.secretname_without_rotation)
 
         # Then
+        self.assertFalse(result)
 
+    def test_should_return_true_when_is_rotation_enabled_given_secret_has_rotation_enabled(self):
+        # Given
+
+        # When
+        result = AwsSecretsManagerService(self.secretsmanager).is_rotation_enabled(self.secretname_with_rotation)
+
+        # Then
+        self.assertTrue(result)
