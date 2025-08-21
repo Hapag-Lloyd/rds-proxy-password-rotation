e2c03affb78b1fa595ef17d77c13daa214bfec5e
Matthias Kay <matthias.kay@hlag.com>
2025-08-21 11:16:11 +0200

    refactor: remove unused import from services.py

--- src/rds_proxy_password_rotation/services.py
+++ src/rds_proxy_password_rotation/services.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from rds_proxy_password_rotation.model import DatabaseCredentials, PasswordStage, UserCredentials, Credentials, RotationStep
+from rds_proxy_password_rotation.model import DatabaseCredentials, PasswordStage, UserCredentials, Credentials
 
 
 class PasswordService(ABC):
