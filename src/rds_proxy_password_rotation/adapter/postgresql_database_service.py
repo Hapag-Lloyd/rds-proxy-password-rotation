5ac94f067711abf6c25b0c0425267bf8bf336c63
Matthias Kay <matthias.kay@hlag.com>
2025-08-30 12:48:27 +0200

    fix lint errors

--- src/rds_proxy_password_rotation/adapter/postgresql_database_service.py
+++ src/rds_proxy_password_rotation/adapter/postgresql_database_service.py
@@ -11,22 +11,17 @@ class PostgreSqlDatabaseService(DatabaseService):
         self.logger = logger
 
     def test_user_credentials(self, credentials: DatabaseCredentials) -> bool:
-        with self.__get_connection(credentials) as conn:
+        with self._get_connection(credentials) as conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT 1")
 
                 return True
 
     def change_user_credentials(self, old_credentials: DatabaseCredentials, new_password: str):
-        with self.__get_connection(old_credentials) as conn:
+        with self._get_connection(old_credentials) as conn:
             with ClientCursor(conn) as cur:
                 cur.execute(sql.SQL("ALTER USER {} WITH PASSWORD %s").format(sql.Identifier(old_credentials.username)), (new_password,))
                 conn.commit()
-
-    def __get_connection(self, credentials: DatabaseCredentials) -> Connection:
-        connect_string = (f'dbname={credentials.database_name} sslmode=require port={credentials.database_port}'
-                          f' user={credentials.username} host={credentials.database_host}'
-                          f' password={credentials.password} connect_timeout=3')
 
     def _get_connection(self, credentials: DatabaseCredentials) -> Connection:
         """
--- tests/rds_proxy_password_rotation/test_password_rotation_application_infra.py
+++ tests/rds_proxy_password_rotation/test_password_rotation_application_infra.py
@@ -1,4 +1,5 @@
 import os
+import random
 import uuid
 
 from unittest import TestCase
